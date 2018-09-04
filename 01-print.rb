252572811decef7e2945e89fc617b9c615a3f6cf
root <root@chefdk.c.first-191315.internal>
2018-09-04 16:04:28 +0000

    D1 ruby

---
+++ 01-print.rb
@@ -0,0 +1,5 @@
+#!/bin/ruby
+
+puts " Ruby Programming for Chef"
+
+# puts is like print in ruby
---
+++ 02-variables.rb
@@ -0,0 +1,21 @@
+#!/bin/ruby
+
+x = 1
+a = 2
+#string has to be single or double quotes"
+name = 'PPK'
+puts x 
+
+# way of accessing variables
+
+puts "x values is: #{x}"
+puts a
+puts "a value is: #{a}"
+puts name
+puts "string var value is: #{name}"
+#Math
+puts x+a
+puts "sum of 2 no's is: #{x+a}"
+
+
+
